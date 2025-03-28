f9acb069edaa62bb4d6e2f78a0e91390c1a0e0d8
RaulADSI <rauramirez25@gmail.com>
2025-03-28 10:31:51 -0500

    Cambio context path

---
+++ core/src/main/java/edu/tienda/core/controllers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,29 @@
+package edu.tienda.core.controllers;
+
+
+import edu.tienda.core.exceptions.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        body.put("message", ex.getMessage());
+        body.put("Error", HttpStatus.BAD_REQUEST.toString());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
