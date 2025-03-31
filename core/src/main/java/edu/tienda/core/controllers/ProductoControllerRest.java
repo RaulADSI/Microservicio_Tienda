69ec660c19475788eca2c043647d8f3cd4a1480d
RaulADSI <rauramirez25@gmail.com>
2025-03-31 14:42:43 -0500

    API externa

--- core/src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
+++ core/src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
@@ -1,15 +1,18 @@
 package edu.tienda.core.controllers;
 
 
+import edu.tienda.core.configurations.ConfigurationParameters;
 import edu.tienda.core.domain.Producto;
 import edu.tienda.core.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,11 +20,30 @@ import java.util.List;
 public class ProductoControllerRest {
 
     @Autowired
+    @Lazy
     private ProductoService productosService;
+
+    @Autowired
+    private ConfigurationParameters configurationParameters;
 
     @GetMapping
     public ResponseEntity<?> getProductos(){
+
+        System.out.println("params " + configurationParameters.toString());
+
         List<Producto> productos = productosService.getProductos();
+
+        return ResponseEntity.ok(productos);
+    }
+
+    @GetMapping("/fake-productos")
+    public ResponseEntity<?> fakeProductosApi(){
+
+        List<Producto> productos = new ArrayList<>(Arrays.asList(
+                new Producto(1, "Camiseta Real Cartagena", 1200.0,4),
+                new Producto(2, "Camiseta America de Cali", 1500.0,8),
+                new Producto(1, "Camiseta Seleccion Colombia", 1800.0,16)
+        ));
         return ResponseEntity.ok(productos);
     }
 }
--- core/src/main/java/edu/tienda/core/domain/Cliente.java
+++ core/src/main/java/edu/tienda/core/domain/Cliente.java
@@ -1,6 +1,12 @@
 package edu.tienda.core.domain;
 
+import lombok.*;
+
 import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Cliente {
 
@@ -8,46 +14,4 @@ public class Cliente {
     private String password;
     private String nombre;
 
-    public Cliente(String username, String password, String nombre) {
-        this.username = username;
-        this.password = password;
-        this.nombre = nombre;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cliente cliente = (Cliente) o;
-        return Objects.equals(username, cliente.username); // Usa el campo único que identifica a un cliente
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username); // Asegúrate de usar el mismo campo
-    }
 }
